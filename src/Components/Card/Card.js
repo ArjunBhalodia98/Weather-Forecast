08a26440ac7f8df1231f13931f53a2cbc384d43a
Arjun Bhalodia - Mac <thisajplays@gmail.com>
2021-08-23 18:24:32 -0400

    Card Finished

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Card, Form, Button, Alert, Container, Row, Col} from 'react-bootstrap';
+import { Card, Col} from 'react-bootstrap';
+import CloudIcon from '@material-ui/icons/Cloud';
 
 export default function WeatherCard(props) {
 
     return (
-        <Col>
+        <Col className="mb-4 d-flex align-items-center flex-column">
             <Card style={{ width: '10rem' }}>
                 <Card.Body>
-                    <Card.Title>{props.day}</Card.Title>
+                    <Card.Title>
+                        {props.day}
+                    </Card.Title>
                     <Card.Text className="d-flex align-items-center flex-column">
                         <div style={{fontWeight: 'bold'}}>{props.text}</div>
                         <div style={{fontStyle: 'italic'}}>{props.Date}</div>
+                        <div> <CloudIcon style={{fontSize: '4rem'}}/></div>
+                        <div className="d-flex" style={{fontSize: '2rem'}}>
+                            <div>32</div>
+                            <div>ºF</div>
+                        </div>
+                        <div>{props.WeatherCondition}</div>
                     </Card.Text>
                 </Card.Body>
             </Card>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import WeatherCard from '../Card/Card';
-import { Card, Form, Button, Alert, Container, Row, Col} from 'react-bootstrap';
+import {Container, Row} from 'react-bootstrap';
 
 export default function Home() {
 
   return (
     <Container>
       <Row xs={1} md={4} lg={5}>
-        <WeatherCard text="Friday" Date="March 1st"/>
-        <WeatherCard text="Saturday" Date="March 2st"/>
-        <WeatherCard text="Sunday" Date="March 3rd"/>
-        <WeatherCard text="Monday" Date="March 4th"/>
-        <WeatherCard text="Tuesday" Date="March 5th"/>
+        <WeatherCard text="Friday" Date="March 1st" WeatherCondition="Sunny"/>
+        <WeatherCard text="Saturday" Date="March 2st" WeatherCondition="Sunny"/>
+        <WeatherCard text="Sunday" Date="March 3rd" WeatherCondition="Sunny"/>
+        <WeatherCard text="Monday" Date="March 4th" WeatherCondition="Sunny"/>
+        <WeatherCard text="Tuesday" Date="March 5th" WeatherCondition="Sunny"/>
       </Row>
     </Container>
   )
